c7325f520f2203dc588656cc5a323ce37c68afe6
hlavki <hlavki@gmail.com>
2008-05-27 13:47:59 +0200

    initial and first release of svuid generator plugin

---
+++ src/eu/easyedu/netbeans/svuid/MethodInfo.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eu.easyedu.netbeans.svuid;
+
+import java.util.Set;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.Name;
+
+/**
+ *
+ * @author hlavki
+ */
+public class MethodInfo extends ElementInfo {
+
+    public MethodInfo(Name name, Set<Modifier> modifiers, String descriptor) {
+        super(name, modifiers, descriptor);
+    }
+
+    @Override
+    public boolean includeInSerialVersionUID() {
+        return (access & OpCodes.ACC_PRIVATE) == 0;
+    }
+    
+    
+    @Override
+    public int getSvuidAccess() {
+        return access;
+    }
+
+}
